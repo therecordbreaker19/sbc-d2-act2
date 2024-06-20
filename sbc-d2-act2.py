6e22e994dfbbfce410f5ff782487797a68a4693f
Jared Burdeos <132859086+therecordbreaker19@users.noreply.github.com>
2024-06-19 17:21:49 -0700

    sbc-d2-act2.py

--- sbc-d2-act2.py
+++ sbc-d2-act2.py
@@ -4,21 +4,21 @@ from random import randint
 user_choice = int(input("Enter 0 (hayang) or 1 (kulob): "))
 
 
-comp1 = randint(0, 1)
-comp2 = randint(0, 1)
+c1 = randint(0, 1)
+c2 = randint(0, 1)
 
 
 choices = ["hayang", "kulob"]
 print(f"Me: {choices[user_choice]}")
-print(f"C2: {choices[comp1]}")
-print(f"C3: {choices[comp2]}")
+print(f"C2: {choices[c1]}")
+print(f"C3: {choices[c2]}")
 
 
-if (user_choice == 0 and comp1 == 1 and comp2 == 1) or (user_choice == 1 and comp1 == 0 and comp2 == 0):
+if (user_choice == 0 and c1 == 1 and c2 == 1) or (user_choice == 1 and c1 == 0 and c2 == 0):
     print("You win!")
-elif (user_choice == 1 and comp1 == 0 and comp2 == 1) or (user_choice == 0 and comp1 == 1 and comp2 == 0):
+elif (user_choice == 1 and c1 == 0 and c2 == 1) or (user_choice == 0 and c1 == 1 and c2 == 0):
     print("C2 wins!")
-elif (user_choice == 1 and comp1 == 1 and comp2 == 0) or (user_choice == 0 and comp1 == 0 and comp2 == 1):
+elif (user_choice == 1 and c1 == 1 and c2 == 0) or (user_choice == 0 and c1 == 0 and c2 == 1):
     print("C2 wins!")
 else:
     print("It's a draw!")
